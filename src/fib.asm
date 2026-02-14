4ff664a3b2441dc90c86e8e5389004b4c3e1b47a
Lexi Mattick <lexi.mattick@gmail.com>
2026-02-14 11:25:25 -0800

    Basic assembler stuff

---
+++ src/fib.asm
@@ -0,0 +1,31 @@
+n = 17
+result = 0
+terms = 0 1
+terms_len = 2
+i = 2
+i_lte_n = 0
+prev1 = 0
+prev2 = 0
+neg1 = -1
+neg2 = -2
+pos1 = 1
+loop_addr = loop
+after_loop_addr = after_loop
+terms_addr = terms
+out = 0
+_start:
+loop:
+    <= i_lte_n i n
+    jmp0 i_lte_n after_loop_addr
+    add prev1 i neg1
+    add prev2 i neg2
+    load prev1 terms_addr prev1
+    load prev2 terms_addr prev2
+    add result prev1 prev2
+    store terms_addr terms_len result
+    add terms_len terms_len pos1
+    add i i pos1
+    jmp loop_addr
+after_loop:
+    load out terms_addr n
+    halt
